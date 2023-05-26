a5195eeb4836e2aef647fe8d3600880000902a23
AlterShow <wind.shadow1004@gmail.com>
2023-05-26 09:08:51 -0400

    update

--- src/pages/CoinOverview.js
+++ src/pages/CoinOverview.js
@@ -38,8 +38,6 @@ function CoinOverview(props) {
         ///Connect Wallet
         // This code will be executed only once, similar to componentDidMount
         //this.interval = setInterval (() => this. fetchCurrencyData (), 60 *1000)    
-        if(!isConnected)  
-          open();
         getCoinData(id,1);
         fetchData();
         return () => {
@@ -51,7 +49,7 @@ function CoinOverview(props) {
     const fetchData = async () => {
       setIsLoading(true);
       const coinListresult = await axios.get(
-          'https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=1h&locale=en&x_cg_pro_api_key=CG-cYLMAXA7qqWnK5RXS8WAw5Jk'
+          'https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h&locale=en&x_cg_pro_api_key=CG-cYLMAXA7qqWnK5RXS8WAw5Jk'
       );
       setCoinLists(coinListresult.data);
       setIsLoading(false);
